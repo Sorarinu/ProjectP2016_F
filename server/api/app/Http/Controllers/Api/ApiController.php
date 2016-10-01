aa0c917fb6478a9d109f228b3bf0d2cef3a54e16
Sorarinu <c01143125b@edu.teu.ac.jp>
2016-10-02 02:17:50 +0900

    [make]ゴリ押しくそコード β

--- server/api/app/Http/Controllers/Api/ApiController.php
+++ server/api/app/Http/Controllers/Api/ApiController.php
@@ -10,17 +10,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Htmldom;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Log;
 use Maknz\Slack\Facades\Slack;
+use App\Library\BookmarkParser;
 
 class ApiController extends Controller
 {
     protected $request;
+    private $fs;
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Filesystem $fs)
     {
         $this->request = $request;
+        $this->fs = $fs;
+        header("Access-Control-Allow-Origin: *");
     }
 
     public function signUp()
@@ -113,6 +120,151 @@ class ApiController extends Controller
 
     public function upload()
     {
+        $filePath = $this->request->file('bmfile')->getRealPath();
+        $tmpTags = '';
+        $i = 0;
+        $id = 0;
+        $isFind = false;
+        $tagId = 0;
+        $tagPrevValue = null;
+        $tagPrevId = null;
+        $tagLists = [];
+        $bookmarkItems = [];
+        $tagListItems = [];
+        $bookmarkJson = [
+            'status' => 'OK',
+            'messages' => 'File Loaded.'
+        ];
+
+        $parser = new BookmarkParser();
+        $bookmarks = $parser->parseFile($filePath);
+
+        //タグごとにまとめる
+        foreach ($bookmarks as $b) {
+            if ($tmpTags === $b['tags'])
+                continue;
+
+            $tmpTags = $b['tags'];;
+            $i++;
+
+            foreach ($bookmarks as $bookmark) {
+                if ($bookmark['tags'] === $tmpTags) {
+                    $bookmarkItems[$i][] = $bookmark;
+                }
+            }
+        }
+
+        //フォルダにID付与
+        foreach ($bookmarkItems as $bookmarkItem) {
+            $tags = explode(',', $bookmarkItem[0]['tags']);
+
+            foreach ($tags as $tag) {
+                if (!isset($tagLists[0])) {
+                    $tagLists[] = [
+                        'tag' => $tag,
+                        'id' => $tagId,
+                    ];
+                    $tagId++;
+                } else {
+                    for ($j = 0; $j < count($tagLists); $j++) {
+                        if ($tag === $tagLists[$j]['tag']) {
+                            $isFind = true;
+                            break;
+                        }
+                        $isFind = false;
+                    }
+
+                    if (!$isFind) {
+                        $tagLists[] = [
+                            'tag' => $tag,
+                            'id' => $tagId,
+                        ];
+
+                        $tagId++;
+                    }
+                }
+            }
+        }
+
+        //ParentID付与
+        foreach ($bookmarkItems as $bookmarkItem) {
+            $tags = explode(',', $bookmarkItem[0]['tags']);
+            $end = end($tags);
+            foreach ($tags as $tag) {
+                if($tag === $end) {
+                    @$tagPrevValue = $tags[(count($tags) - 2)];
+
+                    if (!is_null($tagPrevValue)) {
+                        foreach ($tagLists as $tagList) {
+                            if ($tagList['tag'] === $tagPrevValue) {
+                                $tagPrevId = $tagList['id'];
+                            }
+                        }
+
+                        for ($i = 0; $i < count($tagLists); $i++) {
+                            if ($tagLists[$i]['tag'] === $tag) {
+                                $tagLists[$i]['parent_id'] = $tagPrevId;
+                            }
+                        }
+                    } else {
+                        for ($i = 0; $i < count($tagLists); $i++) {
+                            if ($tagLists[$i]['tag'] === $tag) {
+                                $tagLists[$i]['parent_id'] = null;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach ($tagLists as $tagList) {
+            $tagListItems[] = [
+                'id' => $tagList['id'],
+                'parent_id' => $tagList['parent_id'],
+                'title' => $tagList['tag'],
+                'folder' => true
+            ];
+        }
+
+        $bookmarkJson['bookmark'] = $tagListItems;
+
+        //いい感じにする
+        foreach ($bookmarkItems as $bookmarkItem) {
+            $tags = explode(',', $bookmarkItem[0]['tags']);
+            
+            foreach ($tags as $tag) {
+                if ($tag === end($tags)) {
+                    if (strstr($bookmarkItem[0]['tags'], $tag)) {
+                        foreach ($bookmarkItem as $item) {
+                            foreach ($tagLists as $tagList) {
+                                if($tagList['tag'] === $tag)
+                                    $parent_id = $tagList['id'];
+                            }
+                            
+                            $bookmarkItemAfter[] = [
+                                'id' => $id,
+                                'parent_id' => $parent_id,
+                                'title' => $item['title'],
+                                'detail' => $item['note'],
+                                'reg_date' => $item['time'],
+                                'folder' => false,
+                                'url' => $item['uri']
+                            ];
+
+                            for($i = 0; $i < count($bookmarkJson['bookmark']); $i++) {
+                                if($bookmarkJson['bookmark'][$i]['title'] === $tag) {
+                                    $bookmarkJson['bookmark'][$i]['bookmark'] = $bookmarkItemAfter;
+                                    $id++;
+                                }
+                            }
+                        }
+                        unset($bookmarkItemAfter);
+                    }
+                }
+            }
+        }
+
+        return response()->json($bookmarkJson);
     }
 
     public function export()
--- server/api/config/app.php
+++ server/api/config/app.php
@@ -193,8 +193,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Slack'     => Maknz\Slack\Facades\Slack::class,
-
+        'Slack'     => Maknz\Slack\Facades\Slack::class
+        
     ],
 
 ];
