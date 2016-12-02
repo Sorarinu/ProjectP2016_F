063a7b788be98c9bd57c14292ee69cee585fe30b
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-12-02 17:02:30 +0900

    update scraping.py

--- resemblance/main/similarity/api/search/scraping.py
+++ resemblance/main/similarity/api/search/scraping.py
@@ -2,6 +2,9 @@
 
 from bs4 import BeautifulSoup
 import urllib.request
+import requests
+import urllib.error
+from logging import error
 from conf.constants import *
 
 
@@ -10,25 +13,48 @@ class Scraping(object):
         self.url = url
 
     def get_html(self):
-        html = urllib.request.urlopen(self.url)
-        return html
+        try:
+            html = urllib.request.urlopen(self.url)
+        except urllib.error.HTTPError as e:
+            error('HTTPError = ' + str(e.reason))
+            html = None
+            return html
+        except urllib.request.URLError as e:
+            error('URLError = ' + str(e.reason))
+            html = None
+            return html
+        except requests.RequestException as e:
+            error('H = ' + str(e.response))
+            html = None
+            return html
+        else:
+            return html
 
     def convert_html(self):
         html = self.get_html()
-        soup = BeautifulSoup(html, 'lxml')
-        return soup
+        if html is None:
+            return None
+        else:
+            soup = BeautifulSoup(html, 'lxml')
+            return soup
 
     def get_body_text(self):
         soup = self.convert_html()
         str_list = []
-        for string in soup.stripped_strings:
-            string = repr(string).rstrip("'").lstrip("'")
-            str_list.append(string)
-        return str_list
+        if soup is None:
+            return None
+        else:
+            for string in soup.stripped_strings:
+                string = repr(string).rstrip("'").lstrip("'")
+                str_list.append(string)
+            return str_list
 
     def create_scraping_file(self):
-        with open(BODY_TEXT_FILE, 'w') as file:
-            str_list = self.get_body_text()
-            for strings in str_list:
-                file.write(strings + "\n")
-        return file.name
+        str_list = self.get_body_text()
+        if str_list is None:
+            return None
+        else:
+            with open(BODY_TEXT_FILE, 'w') as file:
+                for strings in str_list:
+                    file.write(strings + "\n")
+            return file.name
