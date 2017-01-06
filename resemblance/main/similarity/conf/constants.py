2a07f5175cb40edeb5cac9c3d9febc9cae108ecc
Kouki Arai <c011401529@edu.teu.ac.jp>
2017-01-06 17:39:07 +0900

    add fork

--- resemblance/main/similarity/conf/constants.py
+++ resemblance/main/similarity/conf/constants.py
@@ -1,3 +1,3 @@
-BODY_TEXT_FILE = '/var/www/html/ProjectP2016_F/resemblance/main/similarity/conf/file/html_body.txt'
-WAKATI_FILE = '/var/www/html/ProjectP2016_F/resemblance/main/similarity/conf/file/wakati.txt'
-MODEL = '/var/www/html/ProjectP2016_F/resemblance/main/similarity/conf/models/sample.model'
+BODY_TEXT_FILE = '/Users/sh07_bell/tmp/ProjectP2016_F/resemblance/main/similarity/conf/file/html_body.txt'
+WAKATI_FILE = '/Users/sh07_bell/tmp/ProjectP2016_F/resemblance/main/similarity/conf/file/wakati.txt'
+MODEL = '/Users/sh07_bell/tmp/ProjectP2016_F/resemblance/main/similarity/conf/models/sample.model'
--- resemblance/main/similarity/similarityApplication.py
+++ resemblance/main/similarity/similarityApplication.py
@@ -1,25 +1,27 @@
 # coding:utf-8
 
 import json
+import os
+import sys
 from functools import wraps
-
-from flask import Flask, abort, request, Response, g
 from flaskext.mysql import MySQL
+from flask import Flask, abort, request, Response
 from api.search.loadModel import LoadModelFlag
-from api.search.scraping import Scraping
+from api.create.scraping import Scraping
 from api.tag.loadModel import LoadModelTag
 
-from api.search.genModel import CreateModel
+from api.create.genModel import CreateModel
 from conf.constants import *
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'test'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'test'
-app.config['MYSQL_DATABASE_DB'] = 'test'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
+
+
+def connnection():
+    cur = mysql.connect().cursor()
+    return cur
 
 
 def consumes(content_type):
@@ -35,58 +37,8 @@ def consumes(content_type):
     return _consumes
 
 
-def connect_db():
-    """Connects to the specific database."""
-    # rv = MySQL.connect(app.config['DATABASE'])
-    # rv.row_factory = MySQL.Row
-    rv = 1
-    return rv
-
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = mysql.connect()
-    return db
-
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
-def request_data_eval():
-    data = {
-        'serachWord': request.json['serachWord'],
-        'bookmark': request.json['bookmark']
-    }
-    return data
-
-
-def add_tag():
-    data = request_data_eval()
-    for bookmark_data in data['bookmark']:
-        page = Scraping(bookmark_data['url']).create_scraping_file()
-        if page is None:
-            pass
-        else:
-            CreateModel(page).create_model()
-            tag = LoadModelTag(MODEL, data['searchWord']).load_model_similar_tag()
-            bookmark_data['tags'] = tag
-    return data
-
-
 def add_flag():
+    print(type(request.json))
     data = request.json
     for bookmark_data in data['bookmark']:
         page = Scraping(bookmark_data['url']).create_scraping_file()
@@ -115,14 +67,21 @@ def similarity_tag():
     return Response(response, mimetype='application/json')
 
 
-@app.route('/api/v1/create-model/', methods=['POST'])
-@consumes('application/json')
-def create_model():
-    data = request_data_eval()
+def add_tag():
+    data = eval(request.json)
     for bookmark_data in data['bookmark']:
         page = Scraping(bookmark_data['url']).create_scraping_file()
         CreateModel(page).create_model()
+        tag = LoadModelTag(MODEL, data['searchWord']).load_model_similar_tag()
+        bookmark_data['tags'] = tag
+    return data
+
+
+def fork():
+    pid = os.fork()
+    if pid == 0:
+        app.run(port=8089, debug=True)
 
 
 if __name__ == '__main__':
-    app.run(port=8089)
+    fork()
