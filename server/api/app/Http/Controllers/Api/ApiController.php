469a2dd2548e18c26beedc4baeb490e3286e2513
Sorarinu <c01143125b@edu.teu.ac.jp>
2017-02-10 13:30:23 +0900

    fix thumbnailSize

--- server/api/app/Http/Controllers/Api/ApiController.php
+++ server/api/app/Http/Controllers/Api/ApiController.php
@@ -406,7 +406,7 @@ class ApiController extends Controller
     public function snap()
     {
         $url = $this->request->input('url');
-        $apiUrl = 'http://capture.heartrails.com/500×500?' . $url;
+        $apiUrl = 'http://capture.heartrails.com/350x300?' . $url;
         $image = Image::make(file_get_contents($apiUrl));
         return $image->response('jpg');
     }
